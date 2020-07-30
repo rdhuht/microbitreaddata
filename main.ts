15104ee4351422fe2db6432fb3088145fd475fe5
JeremyTsui <44908010+rdhuht@users.noreply.github.com>
2020-07-30 22:30:57 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,14 @@
+serial.redirect(
+SerialPin.USB_TX,
+SerialPin.USB_RX,
+BaudRate.BaudRate115200
+)
+serial.redirectToUSB()
 basic.forever(function () {
+    serial.writeLine("" + input.acceleration(Dimension.X) + " " + input.acceleration(Dimension.Y) + " " + input.acceleration(Dimension.Z) + " " + input.buttonIsPressed(Button.A) + " " + input.buttonIsPressed(Button.B))
     basic.pause(200)
     led.plotBarGraph(
     input.acceleration(Dimension.X),
     1024
     )
-    serial.writeString("" + input.acceleration(Dimension.X) + " " + input.acceleration(Dimension.Y) + " " + input.acceleration(Dimension.Z) + " " + input.buttonIsPressed(Button.A) + " " + input.buttonIsPressed(Button.B))
 })
